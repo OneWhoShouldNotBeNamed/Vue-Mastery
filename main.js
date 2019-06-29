ef361e0900caa5cf53f0ef8b21f89e579382fe68
Mahesh SK <maheshsk019@gmail.com>
2019-06-29 22:59:41 +0530

    Donek

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ data:{
     description:"A pair of warm, fuzzy socks.",
     image:"./vmSocks-green-onWhite.jpg",
     altText:"A pair of Socks",
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    onSale:true,
+    inStock:true,
+    details:["80% Cotton","20% polyster","Gender-neutral"],
+    sizes:["5","6","7"],
+    variants:
+    [
+        {
+            variantId:2234,
+            variantColor:"green"
+        },
+        {
+            variantId:2235,
+            variantColor:"blue"
+        }
+    ]
 }
 })
