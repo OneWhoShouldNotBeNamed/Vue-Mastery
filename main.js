2513924029f3af0f5efd401bf749e38c80cb942b
OneWhoShouldNotBeNamed <maheshsk019@gmail.com>
2019-07-01 17:07:36 +0530

    Added Components

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-
-
-var app=new Vue({
-el:'#app',
-data:{
+Vue.component('product',
+{
+    template:`
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+           <div class="product-info">
+            <h1>{{title}}</h1>
+            <p>{{description}}</p>
+            <p v-if="onSale">Item On Sale!!!! Grab Fast!!!</p>
+            <p v-else>Regular Sale</p>
+            <p v-if="inStock">In Stock</p>
+            <p v-else :class ="{outOfStock : !inStock}">Out of Stock</p>
+            <a :href="link" target="_blank">View More</a>
+            <ul>
+                <li v-for="detail in details">{{detail}}</li>
+            </ul>
+            <label for="">Size</label>             <select >
+                <option v-for="size in sizes">
+                       {{size}}
+                </option>
+            </select>
+                
+            <div class="color-box"
+            v-for="(variant, index) in variants" 
+            :key="variant.variantId"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)"
+            >
+       </div> 
+            <div class="cart">
+                <p>Cart({{cart}})</p>
+            </div>
+            <button v-on:click="addToCart"
+            :disabled="!inStock"
+            :class="{disabledButton:!inStock}">Add to Cart</button>
+            <button @click="RemoveCart">Remove From Cart</button>
+            </div>            
+       </div>
+   
+    `,
+    data(){
+   return {
     product:"Socks",
     brand:"Vue Mastery",
     description:"A pair of warm, fuzzy socks.",
@@ -30,19 +69,21 @@ data:{
         }
     ]
    
+    }
+},
+methods:
+{
+    addToCart(){
+        this.cart += 1
     },
-    methods:{
-        addToCart(){
-            this.cart += 1
-        },
-        RemoveCart(){
-            this.cart-=1
-        },
-        updateProduct(index)
-        {
-            this.selectedVariant=index
-          //  console.log(index)
-        }
+    RemoveCart(){
+        this.cart-=1
+    },
+    updateProduct(index)
+    {
+        this.selectedVariant=index
+      //  console.log(index)
+    }
 },
 computed:
 {
@@ -64,5 +105,10 @@ computed:
         } 
           return  this.brand + ' ' + this.product + ' are not on sale'
       }
+
 }
+})
+
+var app=new Vue({
+        el:'#app'
 })
